17d196127bd6da7a48a51b1aa493a25356e9809f
Nico Hidalgo <raulnico123@hotmail.com>
2025-11-12 13:49:18 -0500

    Segundo commit

--- tests/test_procesador.py
+++ tests/test_procesador.py
@@ -5,7 +5,7 @@ class TestAnalizador(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        cls.analizador = Analizador("data/sri_ventas_2024.csv")
+        cls.analizador = Analizador("datos/sri_ventas_2024.csv")
 
     def test_ventas_totales_como_diccionario(self):
         resumen = self.analizador.ventas_totales_por_provincia()
@@ -20,6 +20,13 @@ class TestAnalizador(unittest.TestCase):
         resumen = self.analizador.ventas_totales_por_provincia()
         self.assertTrue(all(float(v) > 5000 for v in resumen.values()))
 
+    ##for venta in resumen.values():
+        ## venta_flotante = float(venta) 
+        ##if venta_flotante <= 5000:
+        ##todas_las_ventas_son_grandes = False 
+        ##break 
+        ##self.assertTrue(todas_las_ventas_son_grandes)
+    
     def test_ventas_por_provincia_inexistente(self):
         with self.assertRaises(KeyError):
             self.analizador.ventas_por_provincia("Narnia")
